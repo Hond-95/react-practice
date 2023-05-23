12e37264af1836d02869b6d133533aafa84979ff
Hond-95 <honda@scottlab.org>
2023-05-23 12:33:33 +0900

    selectBoxComponent

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -48,6 +48,42 @@ const InputText = () => {
   )
 }
 
+// ##################################################################################
+const values = [
+  { id: 1, item: "HTML"},
+  { id: 2, item: "CSS"},
+  { id: 3, item: "JavaScript"},
+]
+
+const SelectedItems = values.map((value) => {
+  return (
+    <option value={value.item} key={value.id}>
+      {value.item}
+    </option>
+  )
+});
+
+const InputSelectBox = () => {
+  const [selectedValue, setSelectedValue] = useState(values[0]['item'])
+  
+  const handleChange = (e) => {
+    setSelectedValue(e.target.value)
+  };
+  
+  return (
+    <div className="App">
+      <p>
+        現在選択されている値：
+        <b>{selectedValue}</b>
+      </p>
+      
+      <select value={selectedValue} onChange={handleChange}>
+        { SelectedItems }
+      </select>
+    </div>
+  )
+}
+
 export default function App() {
-  return <InputText />;
+  return <InputSelectBox />;
 };
